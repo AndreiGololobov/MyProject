349d68e4727fb3a83e92c2c46227c717f2756c72
AndreiGololobov <andrei.gololobov@hotmail.com>
2025-02-22 21:46:23 +0200

    settings .py reg app

--- CarShop/CarShop/settings.py
+++ CarShop/CarShop/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'accounts.apps.AccountsConfig',
+    'cars.apps.CarsConfig',
+    'contacts.apps.ContactsConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
